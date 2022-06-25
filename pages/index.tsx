5056cc167b70c9599365d6e3a891d16857c0721a
aly3n <aly3n.dev@gmail.com>
2022-06-25 21:36:05 +0430

    add font

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,12 +3,16 @@ import styles from "../styles/Home.module.css";
 import { combinationSum, subsetSum, sumOfWhatever } from "../utils/index";
 
 export default function Home() {
-  const [specValue, setSpecValue] = useState(0);
+  const [specValue, setSpecValue] = useState();
+  const [limitValue, setLimitValue] = useState();
   const [rangeOfNumbers, setRangeOfNumbers] = useState([]);
   const formRef = useRef<HTMLFormElement>(null);
 
   const specValueChangeHandler = (e) => {
     setSpecValue(e.target.valueAsNumber);
+  };
+  const limitValueChangeHandler = (e) => {
+    setLimitValue(e.target.valueAsNumber);
   };
   const rangeChangeHandler = (e) => {
     const range = e.target.value.split(/\t|\n/g);
@@ -17,6 +21,7 @@ export default function Home() {
     }).filter(Number);
     setRangeOfNumbers(numberRagne);
   };
+
   const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
     const numberRagne = rangeOfNumbers.map((num)=>{
@@ -32,17 +37,26 @@ export default function Home() {
         className="w-full h-full flex flex-col justify-center items-center gap-5"
       >
         <input
+          onChange={limitValueChangeHandler}
+          type={"number"}
+          value={limitValue}
+          className="w-40 h-10 border-2 border-red-300 rounded-lg p-2 outline-none"
+          placeholder="limit"
+        />
+        <input
           onChange={specValueChangeHandler}
           type={"number"}
           value={specValue}
-          className="w-40 h-10 border-2 border-green-300"
+          className="w-40 h-10 border-2 border-green-300 rounded-lg p-2 outline-none"
+          placeholder="sum"
         />
         <textarea
           onChange={rangeChangeHandler}
           value={rangeOfNumbers}
-          className="w-96 h-96 border-2 border-blue-300"
+          className="w-96 h-96 border-2 border-blue-300 rounded-lg p-2 outline-none"
+          placeholder="range: copy and paste here from excel"
         />
-        <button type={"submit"} className="w-24 h-10 rounded-xl bg-red-500">
+        <button type={"submit"} className="w-24 h-10 rounded-xl bg-green-700 hover:bg-green-900 active:scale-95 text-white duration-300">
           calculate
         </button>
       </form>
